5c1f86b4230b36794dc9142d3e7e7c056748cf86
Maloque18705 <89638352+Maloque18705@users.noreply.github.com>
2024-10-21 15:46:11 +0700

    Upload assets

---
+++ assets/js/slide.js
@@ -0,0 +1,32 @@
+const images = document.getElementsByClassName('img');
+let index = 0;
+let timeoutId;
+
+function showImage(){
+for(let i = 0; i < images.length; i++){
+if(!images[i].classList.contains("hidden")){
+images[i].classList.add("hidden");
+}
+}
+images[index].classList.remove("hidden");
+timeoutId = setTimeout(next, 3000);
+}
+function next(){
+clearTimeout(timeoutId);
+index++;
+if(index >= images.length){
+index = 0;
+}
+showImage();
+}
+
+function prev(){
+clearTimeout(timeoutId);
+index--;
+if(index < 0){
+index = images.length - 1;
+}
+showImage();
+}
+
+showImage();
